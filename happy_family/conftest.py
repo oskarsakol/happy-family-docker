2c6280756d6412151e8949d5da37b231a99b2f99
Oskar Sąkol <sakoloskar@gmail.com>
2020-12-15 09:16:59 +0100

    initial commit

---
+++ happy_family/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from happy_family.users.models import User
+from happy_family.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
